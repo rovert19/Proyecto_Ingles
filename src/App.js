b2c1c53054fc78433bad3f19b0989f19af5e565b
Antony Jaramillo <antony.jaramillo.a@uni.pe>
2023-03-07 10:58:19 -0500

    Se creo el componente Logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Logo } from './components/Logo';
 
 function App() {
   return (
-    <div>
+    <div className='prueba'>
       <h1>Hello world!!!</h1>
+      <Logo/>
     </div>
   );
 }
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,15 @@
+import "./Logo.css";
 
 export const Logo = () => {
   return (
-    <div>Logo</div>
-  )
-}
+    <div className="logo-container">
+      <div className="logo">
+        <span>A</span>
+        <span>
+          <img src={require("../img/logo-icon2.png")} alt="Logo" />
+        </span>
+        <span>L</span>
+      </div>
+    </div>
+  );
+};
